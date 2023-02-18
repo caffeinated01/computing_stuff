1db714d4c4c29ab4c348354e1a2ca905a95c8047
Javier <javier.has.no.life@gmail.com>
2023-02-18 17:16:01 +0800

    add enumerate.py

---
+++ src/examples/enumerate.py
@@ -0,0 +1,11 @@
+# Example usage of enumerate
+"""
+Enumerate takes in two args
+1) Iterable - a sequence, an iterator, or objects that support iteration
+2) Start - (optional) the number of which enumerate starts counting from. If omitted, 0 is taken as start
+Enumerate adds counter to an iterable and returns it. The returned object is an enumerate object.
+"""
+
+a = ["JJC", "NYJC", "TJC", "CJC", "HCI", "RJC", "YIJC"]
+for i, v in enumerate(a):
+    print(i, v)  # i refers to index, while v refers to
