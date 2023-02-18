92e2c143220797a53bdfb9bcd54a560a8b3669d0
Javier <javier.has.no.life@gmail.com>
2023-02-18 18:34:11 +0800

    merge append.py to list_manipulation.py

--- src/examples/list_manipulation.py
+++ src/examples/list_manipulation.py
@@ -7,20 +7,32 @@ example = ["hello"]
 example.append("world")
 print(example)
 
+# Another example of append in a for loop
+string = "hello world"
+example2 = []
+for i in string:
+    example2.append(i)
+# returns ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']
+print(example2)
+
 # You can also use insert() to specify where to add the item.
-example.insert(1, "beautiful")
-print(example)
+example3 = ["hello","world"]
+example3.insert(1, "beautiful")
+print(example3)
 
 # You can also add lists onto lists with .extend(), which causes the items in the second list (the one in the argument/brackets) to be added onto the end of the first list
-example1 = ["world"]
-example.extend(example1)
-print(example)
+example4 = ["hello"]
+word = ["beautiful","world"]
+example4.extend(word)
+print(example4)
 
 # Removing items
 # You can use .remove(), which deletes the first/earliest item which matches your item.
-example.remove("world")
-print(example)
+example5 = ["hello","world","world"]
+example5.remove("world")
+print(example5)
 
 # You can use .pop() to delete an item at the specified index.
-example.pop(1)
-print(example)
+example6 = ["hello","hello","world"]
+example6.pop(1)
+print(example6)
