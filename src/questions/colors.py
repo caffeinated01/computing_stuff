1a81da3b476914e147ba29e8feb2aa33237384ff
Javier <106571756+caffeinated01@users.noreply.github.com>
2023-02-18 18:19:37 +0800

    specify input example

--- src/questions/colors.py
+++ src/questions/colors.py
@@ -2,6 +2,12 @@
 Ask user how many colors they want to input
 Seperate the colors as different items in a list
 Reject the color if it is orange or yellow
+Example input:
+How many colors do you want to input?
+4
+Colors?
+white, orange, black, yellow
+The program would print ['white','black']
 """
 
 num = int(input("How many colors do you want to input?\n"))
