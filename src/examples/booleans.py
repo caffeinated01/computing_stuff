e8ecec115f75d87afa9c81741b95591b2a61f5b8
Javier <javier.has.no.life@gmail.com>
2023-02-18 20:23:46 +0800

    explain "not" in booleans.py

--- src/examples/booleans.py
+++ src/examples/booleans.py
@@ -7,50 +7,57 @@ example2 = False
 # You can create these booleans using evaulations, which creates the booleans based on the inputs you give.
 
 # == means equal.
-print("apple" == "apple") # True
-print("apple" == "orange") # False
+print("apple" == "apple")  # True
+print("apple" == "orange")  # False
 
 # != means NOT equal.
-print("apple" != "orange") # True
-print("apple" != "apple") # False
+print("apple" != "orange")  # True
+print("apple" != "apple")  # False
 
 # > means greater than.
-print(5 > 3) # True
-print(3 > 5) # False
-print(5 > 5) # False
+print(5 > 3)  # True
+print(3 > 5)  # False
+print(5 > 5)  # False
 
 # >= means equal or greater than.
-print(5 >= 5) # True
-print(6 >= 5) # True
+print(5 >= 5)  # True
+print(6 >= 5)  # True
 
 # < means lesser than.
-print(3 < 5) # True
-print(5 < 3) # False
-print(5 < 5) # False
+print(3 < 5)  # True
+print(5 < 3)  # False
+print(5 < 5)  # False
 
 # <= means equal or lesser than.
-print(5 <= 5) # True
-print(4 <= 5) # True
-
-
+print(5 <= 5)  # True
+print(4 <= 5)  # True
 
 # You can reverse a boolean using not().
-print(not(True)) # False
+print(not (True))  # False
 
 # You can use "and" to give True when both its inputs are True.
-print(True and True) # True
-print(True and False) # False
-print(False and False) # False
-print(1 == 1 and 3 > 2) # True
-print(2 != 1 and not(3 <= 3)) # False
+print(True and True)  # True
+print(True and False)  # False
+print(False and False)  # False
+print(1 == 1 and 3 > 2)  # True
+print(2 != 1 and not (3 <= 3))  # False
 
 # You can use "or" to give True when either input is True.
-print(True or False) # True
-print(True or True) # True
-print(False or False) # False
-print(1 != 1 or 1 == 1) # True
+print(True or False)  # True
+print(True or True)  # True
+print(False or False)  # False
+print(1 != 1 or 1 == 1)  # True
 
 # You can combine both "or" and "and" together.
 # It's better to bracket the evaluations to make it more readable.
-print(3 == 4 or (5 < 6 and 6 > 5)) # True
-print(3 == 4 or 5 < 6 and 6 > 5) # Is also True, but a bit harder to understand.
+print(3 == 4 or (5 < 6 and 6 > 5))  # True
+# Is also True, but a bit harder to understand.
+print(3 == 4 or 5 < 6 and 6 > 5)
+
+# You can use "not" to get the opposite of the expected output
+print(not(True)) # False
+print(not(False)) # True 
+# Note that you do not need to put parentheses as "not" is not a function
+# Here are more examples of the usage of "not"
+print(not (1==1 and 1=="1")) #True
+print(not (1!=1 or True)) # False
