9982829a30e99a42dc53dfcbc16dc25e98312be4
Javier <javier.has.no.life@gmail.com>
2023-02-21 17:16:23 +0800

    add stuff in string_manipulation.py

--- src/examples/string_manipulation.py
+++ src/examples/string_manipulation.py
@@ -1,11 +1,22 @@
 # Examples of string manipulation
 # Slicing
-string = "hello beautiful world"
+string = "Hello Beautiful World"
 # Use square brackets to slice. The number infront of the collon is where the slicing starts, the number after is where it ends
-print(string[:5]+" "+string[-5:])  # Returns hello world
+print(string[:5]+" "+string[-5:])  # Returns Hello World
 
 # To reverse a string, use [::-1]
-print(string[::-1])  # Returns dlrow lufituaeb olleh
+print(string[::-1])  # Returns dlroW lufituaeB olleH
 
 # To specify an interval, add another collon behind with the interval
-print(string[::2])  # Returns hlobatflwrd
+print(string[::2])  # Returns HloBatflWrd
+
+# Use .upper() or .lower() to make convert a string to upper or lower case
+print(string.upper())  # Returns HELLO BEAUTIFUL WORLD
+print(string.lower())  # Returns hello beautiful world
+
+# To append each word in a string into a list, use .split()
+print(string.split())  # Returns ['Hello', 'Beautiful', 'World']
+
+# You can specify a parameter in the split() function
+split = "Hello, Beautiful, World"
+print(split.split(", "))  # Returns ['Hello', 'Beautiful', 'World']
