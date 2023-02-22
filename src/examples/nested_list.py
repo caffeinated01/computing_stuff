4cef535d1d472938a35953603512c8ab04a9893e
Javier <javier.has.no.life@gmail.com>
2023-02-22 21:42:43 +0800

    comment the output

--- src/examples/nested_list.py
+++ src/examples/nested_list.py
@@ -1,4 +1,6 @@
 # Example of nested list
 numlist = [[1, 2, 3], [4, 5], [6, 7, 8], 9, 10, [11, 12, 13]]
+# Returns 12
 print(numlist[-1][1])  # Draws the 2nd value of the last nested list in numlist
+# Returns 1
 print(numlist[0][0])  # Draws the 1st value of the first nested list in numlist
--- src/examples/nested_loop.py
+++ src/examples/nested_loop.py
@@ -5,3 +5,14 @@ b = [4, 5, 6]
 for i in a:
     for j in b:
         print(i*j)
+
+# Returns
+# 4
+# 5
+# 6
+# 8
+# 10
+# 12
+# 12
+# 15
+# 18
