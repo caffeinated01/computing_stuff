b24ed6ddf845fdd6fe822449b5ffc7fab8338949
Javier <javier.has.no.life@gmail.com>
2023-02-26 10:15:14 +0800

    generate lists with list() and range()

--- src/examples/list_manipulation.py
+++ src/examples/list_manipulation.py
@@ -3,7 +3,7 @@
 example = ["hello"]
 
 # Adding items
-#You can use .append(), which adds the item to the end of the list.
+# You can use .append(), which adds the item to the end of the list.
 example.append("world")
 print(example)
 
@@ -16,23 +16,27 @@ for i in string:
 print(example2)
 
 # You can also use insert() to specify where to add the item.
-example3 = ["hello","world"]
+example3 = ["hello", "world"]
 example3.insert(1, "beautiful")
 print(example3)
 
 # You can also add lists onto lists with .extend(), which causes the items in the second list (the one in the argument/brackets) to be added onto the end of the first list
 example4 = ["hello"]
-word = ["beautiful","world"]
+word = ["beautiful", "world"]
 example4.extend(word)
 print(example4)
 
 # Removing items
 # You can use .remove(), which deletes the first/earliest item which matches your item.
-example5 = ["hello","world","world"]
+example5 = ["hello", "world", "world"]
 example5.remove("world")
 print(example5)
 
 # You can use .pop() to delete an item at the specified index.
-example6 = ["hello","hello","world"]
+example6 = ["hello", "hello", "world"]
 example6.pop(1)
 print(example6)
+
+# Generate a number list with list() and range()
+example7 = list(range(0, 11))  # Last number not inclusive
+print(example7)  # Returns [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
