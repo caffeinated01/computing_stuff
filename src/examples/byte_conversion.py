05013e65e87446c10a9e73b1b4d765304296a08b
Javier <javier.has.no.life@gmail.com>
2023-02-27 21:39:06 +0800

    add conversion table

--- src/examples/byte_conversion.py
+++ src/examples/byte_conversion.py
@@ -1,3 +1,14 @@
+"""
+Name of unit    Symbol  Size in bytes
+kilobyte        KB      1,000      1000
+kibibyte        KiB     1,024      1024
+megabyte        MB      1,000^2    1,000,000
+mebibyte        MiB     1,024^2    1,048,576
+gigabyte        GB      1,000^3    1,000,000,000
+gibibyte        GiB     1,024^3    1,073,741,824
+terabyte        TB      1,000^4    1,000,000,000,000
+tebibyte        TiB     1,024^4    1,099,511,627,776
+"""
+
 #20 kilobyte to gigabyte
 print((20*1000)/(1000**3))
-
