c233ee0a0b3cd978d7453f4950d999395c606196
caffeinated01 <javier.has.no.life@gmail.com>
2023-03-27 13:56:59 +0800

    today's lesson

---
+++ code/concepts/try_except_else_finally.py
@@ -0,0 +1,18 @@
+# Try, except, else, finally
+
+# Syntax:
+"""
+try:
+       # Some Code.... 
+
+except:
+       # optional block
+       # Handling of exception (if required)
+
+else:
+       # execute if no exception
+
+finally:
+      # Some code .....(always executed)
+"""
+
