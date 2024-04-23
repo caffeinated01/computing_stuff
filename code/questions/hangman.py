1f2fea82af178d93c03772683a4c4eef890416a1
Javier <106571756+caffeinated01@users.noreply.github.com>
2024-04-23 15:55:56 +0800

    Update hangman.py
    add comments to better explain

--- code/questions/hangman.py
+++ code/questions/hangman.py
@@ -3,13 +3,15 @@ from time import sleep
 
 class Game:
     def __init__(self, word):
-        self.mistakes = 0
-        self.turns = 1
-        self.won = False
-        self.word = word
-        self.guessed = []
-        self.state =["_" for i in range(len(self.word))]
-        self.states = [
+        """
+        Initialise variables needed
+        """
+        self.mistakes = 0 # Counts number of mistakes
+        self.turns = 1 # Counts number of tunrs
+        self.won = False # Bool to store whether player has won
+        self.word = word # Word to be guessed
+        self.state =["_" for i in range(len(self.word))] # Initialises a list of all unguessed letters represented by underscores "_"
+        self.states = [ # Ascii art for each time the game progresses
         """
         -------
         |     |
@@ -34,7 +36,7 @@ class Game:
         -------
         |     |
         |     O
-        |    -|-
+        |     |
         |     
         |
         |
@@ -45,7 +47,7 @@ class Game:
         |     |
         |     O
         |    -|-
-        |     |
+        |     
         |
         |
         |-------
@@ -83,9 +85,13 @@ class Game:
         ]
     
     def turn(self):
-        if "".join(self.state) == self.word:
+        """
+        Function to handle guessing and checking if player found letter
+        """
+        if "".join(self.state) == self.word: # If the list of guessed letters joined is equal to word to be guessed, call win 
             self.win()
             return
+        # Print out information of current turn --> no. of mistakes etc.
         print(f"Turn {self.turns}, {5-self.mistakes} chance(s) left")
         print(f"To demoralise you, {self.mistakes} mistake(s) have been made 😈")
         print("Current state of the game:"," ".join(self.state))
@@ -93,6 +99,7 @@ class Game:
         guess = input("Now, you guess: ")
         self.turns += 1
         found_in_turn = False
+        # Replace each underscore with the letter if it is found in the word to be guessed
         for i in range(len(self.word)):
             if self.word[i] == guess:
                 self.state[i] = guess
@@ -106,23 +113,31 @@ class Game:
         self.mistakes += 1
     
     def over(self):
+        """
+        Function called when player loses (i.e. runs out of tries)
+        """
+        print(self.states[self.mistakes])
         print("You have been defeated by immense vocabulary...")
         print(f"Suck it! The word was {self.word} 😂")
     
     def win(self):
+        """
+        Function called when player wins
+        """
         print(self.states[-1])
         print(f"Damn! The word {self.word} wasn't enough to defeat you! How could you have guessed it in {self.turns} turns with only {self.mistakes} mistakes?! NOOOOOOOOOOOOOOOOO 😭!")
         self.won = True
     
     def start(self):
         print("Welcome to Hangman v1269 Alpha Pre-Release ️🤯️")
+        # Main loop that runs forever until a) Player guesses wrongly 5 times or b) Player has won
         while self.mistakes != 5 and self.won == False:
             self.turn()
         if not self.won:
             self.over()
         
-
+# Make new variable with class of Game
 game = Game(random.choice(['babies', 'eagles', 'labels', 'pacify', 'packed', 'oafish', 'iambus']))
 
-
+# Call main function of game
 game.start()
